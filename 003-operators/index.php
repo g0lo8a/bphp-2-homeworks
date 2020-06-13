74979295a80700a47e15e68d74d2a21965558a8c
g0lo8a <c_76@bk.ru>
2020-06-13 13:50:26 +0300

    🔥

---
+++ 003-operators/index.php
@@ -0,0 +1,15 @@
+<?php
+fwrite(STDOUT, 'Введите Ваше имя: ');
+$name = trim(fgets(STDIN, 1024));
+fwrite(STDOUT, 'Введите Вашу фамилию: ');
+$lastName = trim(fgets(STDIN, 1024));
+fwrite(STDOUT, 'Введите Ваше отчество: ');
+$middleName = trim(fgets(STDIN, 1024));
+
+$fullName = mb_convert_case("$lastName $name $middleName", MB_CASE_TITLE);
+$fio = mb_convert_case(mb_substr($lastName, 0, 1) . mb_substr($name, 0, 1) . mb_substr($middleName, 0, 1), MB_CASE_UPPER);
+$surnameAndInitials = mb_convert_case($lastName, MB_CASE_TITLE) . ' ' . mb_strtoupper(mb_substr($name, 0, 1)) . '.' . mb_strtoupper(mb_substr($name, 0, 1));
+
+echo $fullName . PHP_EOL;
+echo $fio . PHP_EOL;
+echo $surnameAndInitials;
